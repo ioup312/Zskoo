2d51338bf3247a6c28f3a1beb70e90766b389735
ioup312 <44092605@qq.com>
2017-06-21 16:37:47 +0800

    1

--- Zskoo/Mainapp/views.py
+++ Zskoo/Mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def main(request):
     context = 'lalalalalal'
-    return render(request,'index.html',{'zhaoxu':context})
+    return render(request, 'index.html', {'zhaoxu': context})
