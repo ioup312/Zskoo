4d9fea4a59c953a66a450389998318c6381c487c
ioup312 <44092605@qq.com>
2017-06-30 12:15:04 +0800

    1

--- Zskoo/Mainapp/models.py
+++ Zskoo/Mainapp/models.py
@@ -2,22 +2,23 @@ from django.db import models
 
 # Create your models here.
 
-
+# 标准主表
 class Zskoo_main(models.Model):
     # id 身份鉴别ID 控制项 行业 S A G
     pass
 
-
+#结果记录表
 class Zskoo_conclusion(models.Model):
     # id 结果记录 简述（数据库模糊查询用） 符合情况 表Aid 表Bid
 
     pass
 
-
+#物理 网络 主机 应用 数据 管理
 class Zskoo_kind(models.Model):
     # id kind
     pass
 
+#A==B
 class Zakoo_main_kind(models.Model):
     # A==B
     pass
