46cc50a713f3df8b3f967e26204633986015a739
ioup312 <44092605@qq.com>
2017-06-30 11:17:27 +0800

    creat sheet

--- Zskoo/Mainapp/models.py
+++ Zskoo/Mainapp/models.py
@@ -4,5 +4,20 @@ from django.db import models
 
 
 class Zskoo_main(models.Model):
+    # id 身份鉴别ID 控制项 行业 S A G
+    pass
 
+
+class Zskoo_conclusion(models.Model):
+    # id 结果记录 简述（数据库模糊查询用） 符合情况 表Aid 表Bid
+
+    pass
+
+
+class Zskoo_kind(models.Model):
+    # id kind
+    pass
+
+class Zakoo_main_kind(models.Model):
+    # A==B
     pass
