9584330f45a15afad039961d68ebf0bef8a16275
ioup312 <44092605@qq.com>
2017-07-01 21:45:13 +0800

    1

--- Zskoo/Mainapp/views.py
+++ Zskoo/Mainapp/views.py
@@ -6,6 +6,11 @@ from Mainapp.models import Zskoo_Main, Zskoo_Conclusion
 
 
 def main(request):
-    a = Zskoo_Conclusion.objects.get()
-    b = a.main_id.zskoo_kind_id
-    return render(request, 'index.html', {'zhaoxu': b})
+    main_data = Zskoo_Main.objects.get()
+    conclusion_data = Zskoo_Conclusion.objects.get()
+    kind = main_data.zskoo_kind
+    point = main_data.ctrl_point_id
+    ctrl_ob = main_data.ctrl_object
+    result = conclusion_data.result_record
+    conformity = conclusion_data.conformity
+    return render(request, 'index.html', {'kind': kind, 'point': point, 'ctrl_ob': ctrl_ob, 'result': result, 'conformity': conformity})
