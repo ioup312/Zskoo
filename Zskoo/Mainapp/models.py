5fd792fceab903c1b90da5761b15638afec069af
ioup312 <44092605@qq.com>
2017-07-01 23:22:28 +0800

    1

--- Zskoo/Mainapp/models.py
+++ Zskoo/Mainapp/models.py
@@ -6,14 +6,14 @@ from django.db import models
 
 # 物理 网络 主机 应用 数据 管理
 class Zskoo_Kind(models.Model):
-    security_object = models.CharField(max_length=100)   # 安全类  exp：网络 主机 应用
+    security_object = models.CharField(max_length=100, default='null')   # 安全类  exp：网络 主机 应用
 
     def __str__(self):
         return self.security_object
 
 
 class Zskoo_Ctrl_Point(models.Model):
-    ctrl_point = models.CharField(max_length=100)   # 控制点  exp：身份鉴别 访问控制
+    ctrl_point = models.CharField(max_length=100, default='null')   # 控制点  exp：身份鉴别 访问控制
 
     def __str__(self):
         return self.ctrl_point
@@ -21,7 +21,7 @@ class Zskoo_Ctrl_Point(models.Model):
 
 class Zskoo_Conclusion(models.Model):
     result_record = models.TextField(default='null')  # 结果记录
-    conformity = models.CharField(max_length=100)  # 符合情况
+    conformity = models.CharField(max_length=100, default='null')  # 符合情况
     resume = models.TextField(default='null')  # 简述
 
     def __str__(self):
@@ -33,7 +33,7 @@ class Zskoo_Main(models.Model):
     ctrl_point_id = models.ForeignKey(Zskoo_Ctrl_Point, on_delete=models.CASCADE, null=True)   # 控制点
     condusion_id = models.ForeignKey(Zskoo_Conclusion, on_delete=models.CASCADE, null=True)
     ctrl_object = models.TextField(default='null')   # 控制项
-    industry = models.CharField(max_length=100)   # 行业分类
+    industry = models.CharField(max_length=100, default='null')   # 行业分类
     S = models.CharField(max_length=20)
     A = models.CharField(max_length=20)
     G = models.CharField(max_length=20)
--- Zskoo/Mainapp/views.py
+++ Zskoo/Mainapp/views.py
@@ -7,10 +7,9 @@ from Mainapp.models import Zskoo_Main, Zskoo_Conclusion
 
 def main(request):
     main_data = Zskoo_Main.objects.get()
-    conclusion_data = Zskoo_Conclusion.objects.get()
     kind = main_data.zskoo_kind
     point = main_data.ctrl_point_id
     ctrl_ob = main_data.ctrl_object
-    result = conclusion_data.result_record
-    conformity = conclusion_data.conformity
+    result = main_data.condusion_id.result_record
+    conformity = main_data.condusion_id.conformity
     return render(request, 'index.html', {'kind': kind, 'point': point, 'ctrl_ob': ctrl_ob, 'result': result, 'conformity': conformity})
