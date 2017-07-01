529eac9900fa7b6beccc801938e48ea3d71eb2d3
ioup312 <44092605@qq.com>
2017-07-01 20:34:58 +0800

    1

--- Zskoo/Mainapp/views.py
+++ Zskoo/Mainapp/views.py
@@ -7,6 +7,5 @@ from Mainapp.models import Zskoo_Main, Zskoo_Conclusion
 
 def main(request):
     a = Zskoo_Conclusion.objects.get()
-    b = a.main_id.ctrl_object
-    context = b
-    return render(request, 'index.html', {'zhaoxu': context})
+    b = a.main_id.zskoo_kind_id
+    return render(request, 'index.html', {'zhaoxu': b})
