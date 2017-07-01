3110aa3f12ac45709e2f6b43ecb4eca734848097
ioup312 <44092605@qq.com>
2017-07-01 19:04:10 +0800

    1

--- Zskoo/Mainapp/views.py
+++ Zskoo/Mainapp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from Mainapp.models import Zskoo_Main
+from Mainapp.models import Zskoo_Main, Zskoo_Conclusion
+
 
 # Create your views here.
 
 
 def main(request):
-    a = Zskoo_Main.objects.get()
-    b = a.industry
+    a = Zskoo_Conclusion.objects.get()
+    b = a.main_id.ctrl_object
     context = b
     return render(request, 'index.html', {'zhaoxu': context})
