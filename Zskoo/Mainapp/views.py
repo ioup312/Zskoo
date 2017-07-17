b8a8e11fc48dbde2ebfed9279751013885149ade
ioup312 <44092605@qq.com>
2017-07-17 19:12:54 +0800

    1

--- Zskoo/Mainapp/views.py
+++ Zskoo/Mainapp/views.py
@@ -6,10 +6,6 @@ from Mainapp.models import Zskoo_Main, Zskoo_Conclusion
 
 
 def main(request):
-    main_data = Zskoo_Main.objects.raw('select * from Mainapp_Zskoo_Main')
+    main_data = Zskoo_Conclusion.objects.raw('select * from Mainapp_Zskoo_Conclusion')
         #print(main_data)
-    # kind = main_data.zskoo_kind
-    # point = main_data.ctrl_point_id
-    # ctrl_ob = main_data.ctrl_object
-    # return render(request, 'index.html', {'kind': kind, 'point': point, 'ctrl_ob': ctrl_ob})
     return render(request, 'index.html', {'main_data': main_data})
